45108a912e6f5404698f679c1b8091bfec9d9bfb
calidummagma-code <calidummagma@gmail.com>
2026-08-05 20:32:39 +0200

    corrige import News

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import { supabase } from "../supabase/client";
 
-import "./News.css";
+import "./news.css";
 
 import logo from "../assets/logo-calidum.png";
 
