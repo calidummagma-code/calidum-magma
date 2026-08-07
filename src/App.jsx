f2df651931f9cdb60bb9fdba85c58b6667f8cfcb
calidummagma-code <calidummagma@gmail.com>
2026-08-07 20:04:41 +0200

    Corregir rutas App y ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -6,103 +6,115 @@ import Discografia from "./components/Discografia";
 import Bolos from "./components/Bolos";
 import Galeria from "./components/Galeria";
 import Contacto from "./components/Contacto";
+
 import EPK from "./pages/EPK";
 import Banda from "./pages/Banda";
 import Bio from "./pages/Bio";
 import Music from "./pages/Music";
 import Directe from "./pages/Directe";
+
 import Admin from "./pages/Admin";
 import News from "./pages/news";
 
-
+import ScrollToTop from "./components/ScrollToTop";
 
 
 export default function App() {
 
     return (
 
-        <Routes>
+        <>
 
-            {/* PÁGINA PRINCIPAL */}
-            <Route
-                path="/"
-                element={<Home />}
-            />
+            <ScrollToTop />
 
-            <Route
-                path="/home"
-                element={<Home />}
-            />
-
-            {/* SECCIONES */}
-
-            <Route 
-             path="/news" 
-                element={<News />} 
-            />
-
-            <Route
-                path="/discografia"
-                element={<Discografia />}
-            />
-
-            <Route
-                path="/bolos"
-                element={<Bolos />}
-            />
-
-            <Route
-                path="/galeria"
-                element={<Galeria />}
-            />
-
-            <Route
-                path="/contacto"
-                element={<Contacto />}
-            />
-<Route path="/banda" element={<Banda />} />
-           
-           
-           <Route
-    path="/admin"
-    element={<Admin />}
-/>
-           
-           
-           
-           
-        
-<Route
-    path="/music"
-    element={<Music />}
-/>
-<Route
-    path="/directe"
-    element={<Directe />}
-/>
+            <Routes>
 
 
+                {/* PÁGINA PRINCIPAL */}
+
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+
+                <Route
+                    path="/home"
+                    element={<Home />}
+                />
 
 
-<Route
-    path="/biografia"
-    element={<Bio />}
-/>
-            
-<Route
-    path="/epk"
-    element={<EPK />}
-/>
+                {/* SECCIONES */}
 
-   {/* SI LA RUTA NO EXISTE */}
-            <Route
-                path="*"
-                element={<Home />}
-            />
+                <Route
+                    path="/news"
+                    element={<News />}
+                />
+
+                <Route
+                    path="/discografia"
+                    element={<Discografia />}
+                />
+
+                <Route
+                    path="/bolos"
+                    element={<Bolos />}
+                />
+
+                <Route
+                    path="/galeria"
+                    element={<Galeria />}
+                />
+
+                <Route
+                    path="/contacto"
+                    element={<Contacto />}
+                />
+
+                <Route
+                    path="/banda"
+                    element={<Banda />}
+                />
+
+                <Route
+                    path="/biografia"
+                    element={<Bio />}
+                />
+
+                <Route
+                    path="/music"
+                    element={<Music />}
+                />
+
+                <Route
+                    path="/directe"
+                    element={<Directe />}
+                />
+
+                <Route
+                    path="/epk"
+                    element={<EPK />}
+                />
 
 
+                {/* ADMIN */}
 
-        </Routes>
+                <Route
+                    path="/admin"
+                    element={<Admin />}
+                />
+
+
+                {/* SI LA RUTA NO EXISTE */}
+
+                <Route
+                    path="*"
+                    element={<Home />}
+                />
+
+
+            </Routes>
+
+        </>
 
     );
 
